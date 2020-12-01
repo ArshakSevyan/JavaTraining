2eb87678e0311de7fcdf0c481f9ac590ec8e9b48
Arshak Sevyan <arshak.sevyan@webbfontaine.com>
2020-12-02 00:48:52 +0400

    String

--- src/FunctionsAndObjects/PersonsX/PersonsAPI.java
+++ src/FunctionsAndObjects/PersonsX/PersonsAPI.java
@@ -6,6 +6,15 @@ package FunctionsAndObjects.PersonsX;
 public class PersonsAPI {
     public static void oldestPerson(Person[] persons) {
         int oldestAge = 0;
+        /**
+         for (int i = 0; i < persons.length; i++) {
+         Person person = persons[i];
+         if (person.getAge() > oldestAge) {
+         oldestAge = person.getAge();
+         nameOfOldestPerson = person.getName();
+         }
+         }*/
+
         String nameOfOldestPerson = " ";
         for (Person person : persons) {
             if (person.getAge() > oldestAge) {
@@ -14,32 +23,30 @@ public class PersonsAPI {
             }
 
         }
-        System.out.println("The oldest person is "+" "+ nameOfOldestPerson);
-
+       System.out.println("The oldest person is " + " " + nameOfOldestPerson);
 
     }
 
-    public static void youngestPerson(Person[] persons){
+    public static void youngestPerson(Person[] persons) {
         int young = persons[0].getAge();
-        String youngestName=" ";
-        for (Person person:persons){
-            if (person.getAge()<young){
-                young=person.getAge();
-                youngestName=person.getName();
+        String youngestName = " ";
+        for (Person person : persons) {
+            if (person.getAge() < young) {
+                young = person.getAge();
+                youngestName = person.getName();
             }
         }
         System.out.println("The youngest person is " + " " + youngestName);
 
     }
-    public static void byCompany(Person[] persons,String companyName){
-        for (Person person:persons){
-            if (person.company.getName().equals(companyName)){
+
+    public static void byCompany(Person[] persons, String companyName) {
+        for (Person person : persons) {
+            if (person.company.getName().equals(companyName)) {
                 System.out.println(person);
             }
         }
     }
-
-
 
 
 }
